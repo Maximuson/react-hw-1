312459a805257723f1a9413af5e5cbd0fc2635d6
Maxim Pavlenko <maxim@MacBook-Air.local>
2020-01-21 01:59:34 +0200

    first task

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styles from './profile.module.css'
+
+const Profile = ({user}) => {
+    
+    return (
+        <div className={styles.profile}>
+        <div class="description">
+            <img
+            src="https://i.pinimg.com/originals/a0/40/66/a04066a2d1fcf25df39c599e093995c8.jpg"
+            alt="user avatar"
+            class={styles.avatar}
+            />
+            <p className={styles.name}>{user.name}</p>
+            <p className={styles.tag}>@{user.tag}</p>
+            <p className={styles.location}>{user.location}</p>
+        </div>
+
+        <ul className={styles.stats}>
+            <li>
+                <span className={styles.label}>Followers</span>
+                <span className={styles.quantity} >{user.stats.followers.toLocaleString()}</span>
+            </li>
+            <li>
+                <span className={styles.label}>Views</span>
+                <span className={styles.quantity}>{user.stats.views.toLocaleString()}</span>
+            </li>
+            <li>
+                <span className={styles.label}>Likes</span>
+                <span className={styles.quantity}>{user.stats.likes.toLocaleString()}</span>
+            </li>
+        </ul>
+        </div>
+    );
+};
+
+export default Profile;
